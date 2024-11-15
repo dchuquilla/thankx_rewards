a37e093ef409d235a59723f8c5b88702b8d6e337
dchuquilla <dario.chuquilla@gmail.com>
2024-11-15 16:03:10 -0500

    Seed database with 50 users using Faker; set default values for name, password, and reward points

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+# Create users
+(1..50).each do |n|
+  member = User.find_or_create_by(email: "user_#{n}@thanx.com") do |user|
+    user.name = "User"
+    user.password = "password"
+    user.password_confirmation = "password"
+    user.reward_points = rand(100..1000)
+  end
+  member.save!
+end
