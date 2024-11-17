d73c48a347aa212852dc27bd81288fdc77dfafd3
dchuquilla <dario.chuquilla@gmail.com>
2024-11-17 04:01:55 -0500

    Add Redemption model and establish associations with User and Reward models; implement validations for Reward attributes

--- backend/app/models/reward.rb
+++ backend/app/models/reward.rb
@@ -1,2 +1,10 @@
 class Reward < ApplicationRecord
+  has_many :redemptions, dependent: :destroy
+  has_many :users, through: :redemptions
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :points_cost, presence: true
+  validates :points_cost, numericality: { greater_than: 0 }
+  validates :points_cost, numericality: { only_integer: true }
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
+  has_many :redemptions, dependent: :destroy
+  has_many :rewards, through: :redemptions
 end
