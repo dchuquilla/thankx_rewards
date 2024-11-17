0a4ab5e603a8ad55aa1a0241ad32e4ae07d5f010
dchuquilla <dario.chuquilla@gmail.com>
2024-11-17 04:35:40 -0500

    Add routes for user profile and points balance endpoint

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'prodile/index'
   mount Rswag::Ui::Engine => "/api-docs"
   mount Rswag::Api::Engine => "/api-docs"
 
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+  namespace :v1 do
+    namespace :users do
+      get "points_balance", to: "profile#index"
+    end
+  end
 end
